08938051044de5248fc20a3bbfd15cc8d7046c00
yamachan0625 <napoli.080625@gmail.com>
2020-08-11 18:29:06 +0900

    #7 commit to

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema, GraphQLSchema } = require('graphql');
+const mongoose = require('mongoose');
+const graphQlSchema = require('./graohql/schema/index');
+const graphQlResolvers = require('./graohql/resolvers/index');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
+    graphiql: true,
+  })
+);
+
+mongoose
+  .connect(
+    // `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@portfolio.tzurq.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@portfolio-shard-00-00.tzurq.mongodb.net:27017,portfolio-shard-00-01.tzurq.mongodb.net:27017,portfolio-shard-00-02.tzurq.mongodb.net:27017/${process.env.MONGO_DB}?ssl=true&replicaSet=atlas-refh68-shard-0&authSource=admin&retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log('ok!!!');
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log('dbエラー');
+  });
---
+++ graohql/resolvers/index.js
@@ -0,0 +1,98 @@
+const bcrypt = require('bcryptjs');
+
+const Event = require('../../models/event');
+const User = require('../../models/user');
+
+const events = async (eventIds) => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    events.map((event) => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event._doc.creator),
+      };
+    });
+    return events;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map((event) => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          date: new Date(event._doc.date).toISOString(),
+          creator: user.bind(this, event._doc.creator),
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async (args) => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: '5f31736b0e4be32db6356650',
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = {
+        ...result._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, result._doc.creator),
+      };
+      const creator = await User.findById('5f31736b0e4be32db6356650');
+      if (!creator) {
+        throw new Error('User exist already.');
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  },
+  createUser: async (args) => {
+    try {
+      const existingUser = await User.findOne({ email: args.userInput.email });
+      if (existingUser) {
+        throw new Error('User exist already.');
+      }
+      const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+      const user = new User({
+        email: args.userInput.email,
+        password: hashedPassword,
+      });
+      const result = await user.save();
+      return { ...result._doc, _id: result.id, password: null };
+    } catch (err) {
+      throw err;
+    }
+  },
+};
