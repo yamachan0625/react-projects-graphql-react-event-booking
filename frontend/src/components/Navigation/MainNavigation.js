87d9fa37e515de4ec338d7f12dd73587123d28be
yamachan0625 <napoli.080625@gmail.com>
2020-08-12 06:09:59 +0900

    #12 adding nav bar

---
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './MainNavigation.css';
+
+const MainNavigation = (props) => {
+  return (
+    <header className="main-navigation">
+      <div className="main-navigation__logo">
+        <h1>Easy Event</h1>
+      </div>
+      <nav className="main-navigation__item">
+        <ul>
+          <li>
+            <NavLink to="/auth">Authenticaticate</NavLink>
+          </li>
+          <li>
+            <NavLink to="/events">Events</NavLink>
+          </li>
+          <li>
+            <NavLink to="/bookings">Bookings</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
