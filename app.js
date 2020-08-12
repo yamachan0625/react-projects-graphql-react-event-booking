c3d2c6bf1784b1d9a9b027b134e696b3ddfab2c9
yamachan0625 <napoli.080625@gmail.com>
2020-08-12 16:50:43 +0900

    #13 hitting api

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema, GraphQLSchema } = require('graphql');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const graphQlSchema = require('./graohql/schema/index');
 const graphQlResolvers = require('./graohql/resolvers/index');
 const isAuth = require('./middleware/is-auth');
@@ -10,6 +10,17 @@ const isAuth = require('./middleware/is-auth');
 const app = express();
 
 app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   if (req.method === 'OPTIONS') {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
+app.use(cors());
 
 app.use(
   '/graphql',
